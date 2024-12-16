13d7603fd481c09cfdb782191f82236271d21387
Jitendra Damor <jitendradamor14@gmail.com>
2024-12-16 11:24:56 +0530

    Plugin information changes
    Plugin information changes

--- elementor-woocommerce-category/elementor-woocommerce-category.php
+++ elementor-woocommerce-category/elementor-woocommerce-category.php
@@ -1,19 +1,20 @@
 <?php
 /**
- * Plugin Name: Elementor WooCommerce Category
+ * Plugin Name: Elementor WooCommerce Category Addon
  * Description: WooCommerce Categories listing with images for Elementor.
  * Version:     1.0.0
- * Author:      Elementor Developer
- * Author URI:  https://developers.elementor.com/
+ * Author:      
+ * Author URI:  
  * Text Domain: elementor-addon
  *
- * Requires Plugins: elementor
- * Elementor tested up to: 3.24.0
- * Elementor Pro tested up to: 3.24.0
+ * Requires Plugins: elementor, woocommerce
+ * Elementor tested up to: 3.25.11
+ * Elementor Pro tested up to: 3.25.4
+ * WooCommerce tested up to: 9.4.3
  */
 
 function register_woocommerce_categories_widget( $widgets_manager ) {
-	require_once( __DIR__ . '/widgets/woocommerce-categories.php' );
+	require_once( __DIR__ . '/woocommerce-categories.php' );
 	$widgets_manager->register( new \WooCommerce_Categories() );
 }
 add_action( 'elementor/widgets/register', 'register_woocommerce_categories_widget' );
